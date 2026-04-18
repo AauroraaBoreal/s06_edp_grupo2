588251d1d7757ff586fd55adfe15cb109b7919ce
Erick Jair Espinoza Mayta <erick-jem@hotmail.com>
2026-04-18 03:49:10 +0000

    app

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from supabase import create_client, Client
+
+# 🔐 Credenciales (pon las tuyas)
+SUPABASE_URL = "https://mljzyorzsdiyduooewif.supabase.co"
+SUPABASE_KEY = "sb_publishable_pmNBv5qI-rkONPTrUjXaMw_4sd7NRWe"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+st.title("🏦 Simulador de Crédito - Banco Regional Andino")
+
+st.write("Obtén una pre-aprobación en minutos")
+
+# 📥 Formulario
+nombre = st.text_input("Nombre")
+edad = st.number_input("Edad", min_value=18, max_value=100)
+ingresos = st.number_input("Ingresos mensuales")
+deudas = st.number_input("Deudas actuales")
+
+if st.button("Evaluar crédito"):
+
+    # 🧠 Lógica simple
+    if ingresos > 2000 and deudas < 0.3 * ingresos:
+        resultado = "Aprobado"
+    else:
+        resultado = "Rechazado"
+
+    # 📊 Mostrar resultado
+    if resultado == "Aprobado":
+        st.success("✅ Crédito pre-aprobado")
+    else:
+        st.error("❌ Requiere evaluación adicional")
+
+    # 💾 Guardar en Supabase
+    data = {
+        "nombre": nombre,
+        "edad": edad,
+        "ingresos": ingresos,
+        "deudas": deudas,
+        "resultado": resultado
+    }
+
+    supabase.table("solicitudes_credito").insert(data).execute()
+
+    st.write("📌 Solicitud guardada")
+
+# 📊 Mostrar historial
+st.subheader("📋 Historial de solicitudes")
+
+response = supabase.table("solicitudes_credito").select("*").execute()
+
+st.dataframe(response.data)
